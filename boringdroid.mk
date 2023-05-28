24b313d6f0f858f48dac8035829dfcdc95666fa5
utzcoz <utzcoz@outlook.com>
2023-05-28 13:14:58 +0800

    Disable boringdroid system apps temporarily
    They will bring compile time's dex2oat verification errors.
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- boringdroid.mk
+++ boringdroid.mk
@@ -4,13 +4,13 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.sys.pcmode.enabled=true \
     persist.sys.systemuiplugin.enabled=true \
 
-PRODUCT_PACKAGES := \
-    BoringdroidSystemUIApk \
-    BoringdroidSettingsApk \
+# PRODUCT_PACKAGES := \
+#     BoringdroidSystemUIApk \
+#     BoringdroidSettingsApk \
 
 # rro overlay
-PRODUCT_PACKAGES += \
-    BoringdroidSystemUIOverlay
+# PRODUCT_PACKAGES += \
+#     BoringdroidSystemUIOverlay
 
 PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml
