7e83822c4d7e033b3e4144af40bd2fa25b72fcd1
utzcoz <utzcoz@outlook.com>
2023-07-01 20:19:11 +0800

    Remove unused resources
    1. Remove unused freeform management xml file as it is the default.
    2. Remove boringdroid PC related system properties as current display
       and OS uses freeform default with PC configuration.
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- boringdroid.mk
+++ boringdroid.mk
@@ -1,8 +1,7 @@
 # Recents
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.recents.grid=true \
-    persist.sys.pcmode.enabled=true \
-    persist.sys.systemuiplugin.enabled=true \
+    persist.sys.systemuiplugin.enabled=true
 
 # PRODUCT_PACKAGES := \
 #     BoringdroidSystemUIApk \
@@ -11,7 +10,3 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # rro overlay
 # PRODUCT_PACKAGES += \
 #     BoringdroidSystemUIOverlay
-
-PRODUCT_COPY_FILES := \
-    frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml
-
