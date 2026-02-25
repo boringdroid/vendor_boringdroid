d89c1da6c78710520631e69d186fcd39a3f8a0a6
utzcoz <utzcoz@outlook.com>
2026-02-26 01:27:23 +0800

    Fix indent in boringdroid.mk
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- boringdroid.mk
+++ boringdroid.mk
@@ -3,9 +3,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.recents.grid=true \
     persist.sys.systemuiplugin.enabled=true
 
- PRODUCT_PACKAGES := \
-     BoringdroidSettingsApk \
-     BoringdroidSystemUIApk \
+PRODUCT_PACKAGES := \
+    BoringdroidSettingsApk \
+    BoringdroidSystemUIApk \
 
 # rro overlay
 # PRODUCT_PACKAGES += \
